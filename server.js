2c6a4eabb58093900da931ad720f5a7ea9b5ff16
Kritika Rag <kritikarag.kr@gmail.com>
2023-10-10 23:50:28 +0530

    modified:   server.js

--- server.js
+++ server.js
@@ -8,6 +8,7 @@ app.use(express.json());
 const connectDB = require('./db');
 require('dotenv').config();
 connectDB();
+
 app.listen(PORT, () => {
     console.log(`Server started on port ${PORT}`);
   });
